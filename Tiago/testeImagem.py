ebfd0cce822a8c9ddfd39d0f6515bf7a5a46beea
Tiago Rodovalho Gonçalves <trodovalhogoncalves@gmail.com>
2025-05-25 20:45:14 -0400

    Add files via upload

--- Tiago/testeImagem.py
+++ Tiago/testeImagem.py
@@ -2,24 +2,14 @@
 import numpy as np
 import cv2
 import time
-# initialize our canvas as a 300x300 pixel image with 3 channels
-# (Red, Green, and Blue) with a black background
-canvas = np.zeros((300, 300, 3), dtype="uint8")
-# draw a green line from the top-left corner of our canvas to the
-# bottom-right
+from cv2 import cvtColor, COLOR_BGR2RGB
+from cv2 import imread
+
+img = imread('verde.png')
+
+print(img[0, 0])
+
 green = (0, 255, 0)
-cv2.circle(canvas, (600, 600), 200, green, thickness= 100, lineType= 8, shift= 2)
-cv2.imshow("Canvas", canvas)
-cv2.waitKey(0)
-
-# draw a 3 pixel thick red line from the top-right corner to the
-# bottom-left
-red = (0, 0, 255)
-cv2.line(canvas, (300, 0), (0, 300), red, 3)
-cv2.imshow("Canvas", canvas)
-cv2.waitKey(0)
-
-blue = (255, 0, 0)
-cv2.line(canvas, (150, 150), (0, 0), blue, 3)
-cv2.imshow("Canvas", canvas)
+#cv2.circle(img, (0, 0), 10, green, thickness= 50, lineType= 8, shift= 2)
+cv2.imshow("Canvas", img)
 cv2.waitKey(0)
