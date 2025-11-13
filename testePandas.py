0524fd7bea96cecc6b72f876ceadabce12094bdd
Murilodevjr <murilofreitas13121@gmail.com>
2025-11-13 16:35:55 -0400

    Analise das tabelas

--- testePandas.py
+++ testePandas.py
@@ -1,4 +1,28 @@
 import pandas as pd
-valor = pd.read_csv("tabelas/ph6,6.csv")
-coluna = valor["temp"]
-print(coluna.head())
+ph = "7,2"
+valor = pd.read_csv(f"tabelas/ph{ph}.csv")
+linha =0
+coluna=0
+ct = 0
+buscaTemp = 25
+buscaAmonia = 0.25
+for i in valor.head():
+
+    print(i, buscaAmonia)
+    l1 = valor[i]
+    linha =0
+    if(buscaAmonia == float(i)):
+        
+        for i2 in l1:
+            print(valor["0"][ct])
+            if(buscaTemp == int(valor["0"][ct])):
+                print("achou!")
+                print(i2)
+                
+            linha+=1
+            ct += 1
+    coluna+=1
+   
+#v = valor.loc[valor["6.5"] == 22, "6.5"]
+#v = valor.iloc[22 , 6.5].values(0)
+#print(v.head())
